3dbe4fc06a97e38e18f7f98b8af0edf38d6ddadb
Achille kouassi <patriceachillekouassin@gmail.com>
2022-12-28 00:12:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const myWatchedSeriesLength = ["black mirror", "money heist", "the big bang theory"];
+const myWatchedSeriesSentence = "la vie de programmeur";
+console.log("I watched 3 series:" + " " + myWatchedSeriesLength + " and the" + myWatchedSeriesSentence);
+
+//PARTIE 2
+myWatchedSeriesLength[2]= "friends"
+console.log(myWatchedSeriesLength)
+
+myWatchedSeriesLength.push("mamita")
+console.log(myWatchedSeriesLength)
+
+myWatchedSeriesLength.unshift("joe")
+console.log(myWatchedSeriesLength)
+
+myWatchedSeriesLength.splice(1, 1);
+console.log(myWatchedSeriesLength)
+
+console.log(myWatchedSeriesLength[1]);
+
+console.log(myWatchedSeriesLength)
+
+
+
